abe66d1aca0232d68bb3dd1a7b96d98eb2d79737
Eircly <32888085+Eircly@users.noreply.github.com>
2022-07-19 14:20:21 +0800

    Add files via upload

--- C++/源.cpp
+++ C++/源.cpp
@@ -72,8 +72,74 @@ int main() {
     }
 
     cout << setiosflags(ios::fixed) << setprecision(1) << cost << endl;*/
+    /*身高
+    double weight;
+    double height;
+
+    cout << "请输入体重";
+    cin >> weight;
+    cout << "请输入身高";
+    cin >> height;
+
+    double BMI = weight / (height * height);
+    if (BMI < 18.5) {
+        cout << "偏瘦" << endl;
+    }
+    else if (BMI >= 18.5 && BMI < 20.9) {
+        cout << "苗条" << endl;
+    }
+    else if (BMI >= 20.9 && BMI < 24.9) {
+        cout << "适中" << endl;
+    }
+    else {
+        cout << "偏胖" << endl;
+    }*/
+    /*成绩
+    int score;
+    cout << "请输入成绩";
+    cin >> score ;
+    if (score < 0 || score>100) cout << "成绩不合法";
+    else if (score >= 90 && score <= 100) cout << "优秀";
+    else if (score >= 80 && score <= 89) cout << "良"; 
+    else if (score >= 70 && score <= 79) cout << "中";
+    else if (score >= 60 && score <= 69) cout << "及格"; 
+    else cout << "差"; */
+    //int month;
+    //cout << "请输入月份";
+    //cin >> month;
+
+    ////3-5月为春季
+    //if (month >= 3 && month <= 5) {
+    //    cout << month << "月是春季" << endl;
+    //}
+    ////6-8月为夏季
+    //else if (month >= 6 && month <= 8) {
+    //    cout << month << "月是夏季" << endl;
+    //}
+    ////9-11月为秋季
+    //else if (month >= 9 && month <= 11) {
+    //    cout << month << "月是秋季" << endl;
+    //}
+    ////12-2月为冬季
+    //else if (month == 12 || month == 1 || month == 2) {
+    //    cout <<month << "月是冬季" << endl;
+    //}
+    ////其它情况，输出不合法
+    //else {
+    //    cout << "不合法" << endl;
+    //}
+    int n;
+    cin >> n;
+    int sum = 0;
+
+    for (int i = 2; i <= n; i++)
+    {
+        if (i % 2 == 0) {
+        sum += i;
+    }
+    }
 
 
-
+    cout << sum << endl;
     return 0;
 }
