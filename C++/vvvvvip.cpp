501f10004ccd784219971c1668e38653f388baff
Eircly <32888085+Eircly@users.noreply.github.com>
2022-08-03 15:13:28 +0800

    Add files via upload

---
+++ C++/vvvvvip.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <deque>
+using namespace std;
+
+class Guest {
+public:
+    string name;
+    bool vip;
+
+    Guest(string name, bool vip) {
+        this->name = name;
+        this->vip = vip;
+    }
+};
+
+int main() {
+
+    Guest guest1("张三", false);
+    Guest guest2("李四", false);
+    Guest vipGuest("王五", true);
+    deque<Guest> deque;
+
+    //将guest1和guest2分别从队尾入队
+    deque.push_back(guest1);
+    deque.push_back(guest2);
+    //将vipGuest从队头入队
+    deque.push_front(vipGuest);
+
+    for (Guest g : deque) {
+        cout << g.name << " ";
+    }
+
+    return 0;
+}
