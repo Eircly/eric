a06d405bb51c0d11a5b9438a552ef1faa2dccd39
Eircly <32888085+Eircly@users.noreply.github.com>
2022-08-10 21:35:57 +0800

    Add files via upload

---
+++ python/2.py
@@ -0,0 +1,25 @@
+#阶梯电价
+# dianliang = int(input())
+# if dianliang < 0:
+#     print("Invalid Value!")
+# elif dianliang <= 50:
+#     cost = dianliang * 0.53
+# else:
+#     cost = 50 * 0.53 + (dianliang - 50) * (0.53 + 0.05)
+# if dianliang >= 0:
+#     print("cost = %.2f" % cost)
+# print('请输入矩阵元素')
+# lst = list(map(int,input().split()))
+# for i in range(0,9,3):
+#     print('{:4d}'.format(lst[i]),end="")
+# print("")
+# for i in range(1,9,3):
+#     print('{:4d}'.format(lst[i]),end="")
+# print("")
+# for i in range(2,9,3):
+#     print('{:4d}'.format(lst[i]),end="")
+# print("")
+
+# a = list(map(int, input().split()))
+# for i in range(3):
+#     print(f"{a[i]:4}{a[i + 3]:4}{a[i + 6]:4}")
