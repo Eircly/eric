cb4293c4405fc6f6dfcfeb16b25a8f1bd50e6c3d
Eircly <32888085+Eircly@users.noreply.github.com>
2022-08-10 22:37:55 +0800

    Create 2.py

--- python/2.py
+++ python/2.py
@@ -23,3 +23,50 @@
 # a = list(map(int, input().split()))
 # for i in range(3):
 #     print(f"{a[i]:4}{a[i + 3]:4}{a[i + 6]:4}")
+import math
+class Complex:
+    def __init__(self):
+        self.x1=0
+        self.x2=0
+        self.y1=0
+        self.y2=0
+        self.x=self.x1-self.x2
+        self.y=self.y1-self.y2
+    def add(self):
+        self.x1=int(input("input x1 "))
+        self.y1=int(input("input y1 "))
+        self.x2=int(input("input x2 "))
+        self.y2=int(input("input y2 "))
+        self.x=self.x1-self.x2
+        self.y=self.y1-self.y2
+    def out(self):
+        print(self.x,self.y)
+    def plus(self,a,b):         #向量相加
+        self.x=a.x+b.x
+        self.y=a.y+b.y
+    def sub(self,a,b):         #向量相减
+        self.x=a.x-b.x
+        self.y=a.y-b.y
+    def pointmuti(self,a,b):   #向量相乘
+        return (a.x*b.x+a.y*b.y)
+    def qiumo(self,m):         #向量求模
+        return(((m.x)**2+(m.y)**2)**0.5)
+v=Complex()
+a=Complex()
+b=Complex()
+a.add()
+b.add()
+print("坐标1:",end="")
+a.out()
+print("坐标2:",end="")
+b.out()
+v.plus(a,b)
+print("向量相加:",end="")
+v.out()
+v.sub(a,b)
+print("向量相减:",end="")
+v.out()
+print("向量相乘:",end="")
+print(v.pointmuti(a,b))
+print("a模值%0.3f"%v.qiumo(a))
+print("b模值%0.3f"%v.qiumo(b))
